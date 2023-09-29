05a564915f539f99e26e44e02938713ea2826298
hamza99113 <alihamza99113@gmail.com>
2023-09-30 00:52:48 +0500

    Testi

--- src/App.js
+++ src/App.js
@@ -1,19 +1,35 @@
-import React, {useContext} from "react";
-import Login from "./components/Login/Login";
-import Home from "./components/Home/Home";
-import MainHeader from "./components/MainHeader/MainHeader";
-import AuthContext from "./store/auth-context";
+import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import Home from './components/home';
+import About from './components/about';
+import Contact from './components/contact';
 
-function App(ssssssssss) {
-  const ctx = useContext(AuthContext);
+function App() {
   return (
-    <React.Fragment>
-      <MainHeader />
-      <main>
-        {!ctx.isLoggedIn && <Login/>}
-        {ctx.isLoggedIn && <Home/>}
-      </main>
-    </React.Fragment>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contact</Link>
+            </li>
+          </ul>
+        </nav>
+      
+        <Routes>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/" element={<Zeht />}></Route>
+          <Route path="/" element={<Homeee />}></Route>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
